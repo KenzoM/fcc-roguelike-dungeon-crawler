8137100bb89a2520628c284dfd55e11f109c5fa5
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-29 16:53:25 -0700

    added psuedo code in reducers

--- src/reducers/reducers_map.js
+++ src/reducers/reducers_map.js
@@ -1,3 +1,36 @@
+//ACTIONS
+
+let initialState = {
+  map : mapGenerator(5,5) //generates map 5 x 5
+  player : {
+    coordinates : null,
+    health : 100 ,
+    weapon : "Stick",
+    expLevel : 1
+  },
+  enemies : [],
+  weapons : [],
+  items : [],
+  occupiedCoordinates = []
+}
+
+function placeEnemy(numberofEnemies){
+  //randomly gets a coordinates from map property
+  //checks if [x][y] is 1 AND it is not placed anything else
+  //. If it is, assign that coordinate to that enemy
+
+}
+
+function mapGenerator(width, height){
+    var result= [];
+    for (var i = 0 ; i < width; i++) {
+        result[i] = [];
+        for (var j = 0; j < height; j++) {
+            result[i][j] = (Math.floor(Math.random() * 2) )
+        }
+    }
+    return result;
+}
 
 
 export default function(state = initialState, action){
