b570f140726b1ef777dc2847936dcd4c0b268fa0
Noel Gonzales <noelalangonzales@gmail.com>
2016-11-02 12:04:12 -0700

    renders items and separates initialState file

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -5,10 +5,15 @@ export default class Tiles extends Component {
     super(props);
   }
   render(){
-    const tile = this.props.cell === 0 ? "wall" : "floor";
+    let tile = this.props.cell === 0 ? "wall" : "floor";
     const row = this.props.row * 50;
     const col = this.props.column * 50;
-    return(
+
+    if (this.props.enemy === 1) { tile = "enemy" }
+    if (this.props.weapon === 1) { tile = "weapon" }
+    if (this.props.item === 1) { tile = "item" }
+
+    return (
       <rect x={row} y={col} width="50" height="50" className={tile}/>
     )
   }
--- src/containers/MapGenerator.js
+++ src/containers/MapGenerator.js
@@ -7,23 +7,48 @@ class MapGenerator extends Component {
   constructor(props){
     super(props);
     this.renderTiles = this.renderTiles.bind(this);
+    this.getCoords = this.getCoords.bind(this);
   }
-  componentDidMount(){
 
-  }
-  renderTiles(cell,row,column){
+
+  renderTiles(cellVal,row,column,enemy,weapon,item) {
     return (
-       <Tiles cell={cell} row={row} column={column} />
+      <Tiles
+        cell={cellVal}
+        row={row}
+        column={column}
+        enemy={enemy}
+        weapon={weapon}
+        item={item}
+      />
     )
+  };
+
+  getCoords(things) {
+    if (this.props.hasOwnProperty(things)) {
+      return this.props[things].map((thing) => (
+        thing.coords[0]+"-"+thing.coords[1]
+      ))
+    }
   }
+
   render(){
-    console.log(this.props.mapGenerated)
+    let enemyCoords = this.getCoords("enemies")
+    let weaponCoords = this.getCoords("weapons")
+    let itemCoords = this.getCoords("items")
+
     return (
       <svg width="500" height="500">
-        {this.props.mapGenerated.grid.map( (row,rowIndex) =>(
-          row.map( (cell, colIndex) => (
-            this.renderTiles(cell,rowIndex,colIndex)
-          ))
+        {this.props.grid.map( (row, rowIndex) =>(
+          row.map( (cellVal, colIndex) => {
+
+            let enemy, weapon, item;
+            enemyCoords.indexOf(rowIndex+"-"+colIndex) > -1 ? enemy = 1 : enemy = 0;
+            itemCoords.indexOf(rowIndex+"-"+colIndex) > -1 ? item = 1 : item = 0;
+            weaponCoords.indexOf(rowIndex+"-"+colIndex) > -1 ? weapon = 1 : weapon = 0;
+
+            return this.renderTiles(cellVal,rowIndex,colIndex,enemy,weapon,item)
+          })
         ))}
       </svg>
     )
@@ -33,9 +58,13 @@ class MapGenerator extends Component {
 // function mapDispatchToProps(dispatch){
 //   return bindActionCreators({onCellClick}, dispatch)
 // }
+
 function mapStateToProps(state) {
   return {
-    mapGenerated: state.mapGenerated
+    grid: state.mapGenerated.grid,
+    enemies: state.mapGenerated.enemies,
+    weapons: state.mapGenerated.weapons,
+    items: state.mapGenerated.items
   };
 }
 
