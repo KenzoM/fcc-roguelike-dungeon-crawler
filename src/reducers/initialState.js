1d677570f1e7a7ce6e66cb889d9ceddc14281ee4
Noel Gonzales <noelalangonzales@gmail.com>
2016-11-07 14:53:45 -0800

    begins refactoring placeThings

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,8 +1,16 @@
 //Function Constuctors..
-function WeaponsConstructor(){
-  this.name = "Weapon Name";
-  this.coords = null;
-  this.damage = 3;
+// function WeaponsConstructor(){
+//   this.name = "Weapon Name";
+//   this.coords = null;
+//   this.damage = 3;
+// }
+
+function createWeapon(name, coords, damage) {
+  return {
+    name: name,
+    coords: coords,
+    damage: damage
+  }
 }
 
 function ItemsConstructor(){
@@ -34,6 +42,7 @@ let initialState = {
 }
 
 initialState.grid = mapGenerator(20,20);
+
 function mapGenerator(width, height){
   var result = [];
   for (var i = 0 ; i < width; i++) {
@@ -50,16 +59,62 @@ function mapGenerator(width, height){
   return result;
 }
 
+
 initialState.items = placeThings("item", 3);
-initialState.weapons = placeThings("weapon", 2)
 initialState.enemies = placeThings("enemy", 4)
+
+
+let weapons = [{name:"knife", damage: 2},{name: "shotgun", damage: 3}]
+
+initialState.weapons = weapons.map( weapon => placeThing("weapon", weapon))
+
+function placeThing(thing, weapon) {
+  let availableCoords = initialState.grid.map( (row, rowIndex) => {
+    return row.map( (cell, colIndex) => {
+      if (cell === 1) {
+        return [rowIndex, colIndex]
+      }
+    })
+  })
+
+  let randCoords = availableCoords[Math.floor(Math.random() * availableCoords.length)];
+
+  switch (thing) {
+    case "enemy": {
+      grid[randCoords[0]][randCoords[1]] = 2;
+      let newEnemy = new EnemyConstructor();
+      newEnemy.coords = coord
+      return newEnemy
+    }
+    case "item": {
+      grid[randCoords[0]][randCoords[1]] = 3;
+      let newItem = new ItemsConstructor();
+      newItem.coords = coord
+      return
+    }
+    case "weapon": {
+      grid[randCoords[0]][randCoords[1]] = 4
+      let newWeapon = createWeapon(weapon.name, randCoords, weapon.damage)
+      newWeapon.coords = coord
+    }
+  }
+
+}
+
+
+
+
 
 initialState.player = {...initialState.player, coords: placeThings("player", 1)}
 
-function placeThings(thing, numberofThings){
+
+
+
+
+function placeThings(thing, numberofThings, weapon){
   let things = []; //placeholder for returning an array of objects of "thing"
   let availableCoords = []; //collects all current possible coords from initialState.grid
-  let placementThings = []; //array of coords that will iterate and assign to each things via switch case
+  let placedThings = []; //array of coords that will iterate and assign to each things via switch case
   let grid = initialState.grid;
 
   grid.forEach( (row, rowIndex) => (
@@ -70,7 +125,7 @@ function placeThings(thing, numberofThings){
     })
   ))
 
-  while(placementThings.length < numberofThings ){
+  while(placedThings.length < numberofThings ){
     let randCoords = availableCoords[Math.floor(Math.random() * availableCoords.length)];
     let codeValue  // 2 means enemy, 3 means item and 4 means weapons;
     if (thing === "enemy"){
@@ -83,12 +138,12 @@ function placeThings(thing, numberofThings){
       codeValue = 0 ;
     }
     if (grid[randCoords[0]][randCoords[1]] === 1){
-      placementThings.push(randCoords);
+      placedThings.push(randCoords);
       grid[randCoords[0]][randCoords[1]] = codeValue;
     }
   }
 
-  placementThings.forEach(coord => {
+  placedThings.forEach(coord => {
     switch (thing){
       case "enemy":
         let newEnemy = new EnemyConstructor();
@@ -96,8 +151,9 @@ function placeThings(thing, numberofThings){
         things.push(newEnemy)
         break;
       case "weapon":
-        let newWeapon = new WeaponsConstructor();
-        newWeapon.coords = coord
+        // let newWeapon = new WeaponsConstructor();
+        // newWeapon.coords = coord
+        let newWeapon = createWeapon(weapon, coord, 2)
         things.push(newWeapon)
         break;
       case "item":
