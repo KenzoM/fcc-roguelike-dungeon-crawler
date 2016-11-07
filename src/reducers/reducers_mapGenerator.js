ab598bdc01d0ae6f9924d1cd8a85bb270e67b6b0
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-07 14:37:05 -0800

    player gains level and increases attack points

--- src/reducers/reducers_mapGenerator.js
+++ src/reducers/reducers_mapGenerator.js
@@ -33,12 +33,16 @@ function attackEnemy(player,enemy){
 }
 
 function didReachNewLevel(statePlayer, expPoints){
-  let diffExp = statePlayer.exp - expPoints;
-  if (diffExp <= 0){
-    return true
-  } else{
-    return false
-  }
+  let diffExp = statePlayer - expPoints;
+  return (diffExp <= 0)
+}
+
+function levelUpPlayer(statePlayer,  currentEXP){
+  let newStatePlayer = {...statePlayer};
+  newStatePlayer.level++; //level up the player
+  newStatePlayer.attack += 5; //level attack 5 points
+  newStatePlayer.exp = 100; //reset back to 100 exp
+  return [newStatePlayer.attack, newStatePlayer.exp, newStatePlayer.level];
 }
 
 const updateGameObject = (state, newCoords) =>{
@@ -51,17 +55,18 @@ const updateGameObject = (state, newCoords) =>{
         return enemy.coords[0] === newCoords[0] && enemy.coords[1] === newCoords[1]
       })
       let resultBattle = attackEnemy(actualPlayer,actualEnemy[0])
-      //resultBattle[0] = player's Health
-      //resultBattle[1] = enemy's Health
       actualEnemy[0].health = resultBattle[1];
 
       if (resultBattle[1] <= 0 && resultBattle[0] > 0){  // case1: if player kills an Enemy
-        //check if players EXP reached to a new level
         let statePlayer;
-        if (didReachNewLevel(state.player, actualEnemy[0].exp)){
-          console.log('true')
+        let newExp = state.player.exp - actualEnemy[0].exp;
+        //check if players EXP reached to a new level
+        if (didReachNewLevel(state.player.exp, actualEnemy[0].exp)){
+          let newLevel = levelUpPlayer(state.player, newExp);
+          statePlayer = {...state.player, health: resultBattle[0],
+                            coords: newCoords, attack: newLevel[0],
+                            exp: newLevel[1], level: newLevel[2] }
         } else{
-          let newExp = state.player.exp - actualEnemy[0].exp;
           statePlayer = {...state.player, health: resultBattle[0],
                             coords: newCoords, exp: newExp }
         }
