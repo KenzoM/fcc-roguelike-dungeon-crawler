157192f3faf297683fc9471bbfa800aaf24118a3
Noel Gonzales <noelalangonzales@gmail.com>
2016-11-09 11:11:03 -0800

    adds dungeon number to status bar

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const StatusBar = (props) => {
   let { health, exp, level, weapon, attack } = props.player
+  let { dungeon } = props
   return (
     <div className="status-bar">
       <ul>
@@ -10,6 +11,7 @@ const StatusBar = (props) => {
         <li>Weapon: {weapon} </li>
         <li>Attack: {attack} </li>
         <li>Next Level: {exp} XP</li>
+        <li>Dungeon: {dungeon}</li>
       </ul>
     </div>
   )
