10ee2972aab7235ab3797f93ae23f821ae29ac0c
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-11 18:46:55 -0800

    changed values of number of rooms and size to avoid infinite while loop

--- src/preload/generator.js
+++ src/preload/generator.js
@@ -7,9 +7,106 @@ let Helpers = {
 
 function Dungeon(){
     this.map= null;
-    this.map_size= 30;
+    this.map_size= 45;
     this.rooms= []
 }
+
+Dungeon.prototype.Generate = function () {
+  this.map = [];
+  for (let x = 0; x < this.map_size; x++) {
+    this.map[x] = [];
+    for (let y = 0; y < this.map_size; y++) {
+        this.map[x][y] = 999;
+    }
+  }
+
+  let room_count = Helpers.GetRandom(5, 10);
+  let min_size = 7;
+  let max_size = 10;
+
+  for (let i = 0; i < room_count; i++) {
+    let room = {};
+
+    room.x = Helpers.GetRandom(1, this.map_size - max_size - 1);
+    room.y = Helpers.GetRandom(1, this.map_size - max_size - 1);
+    room.w = Helpers.GetRandom(min_size, max_size);
+    room.h = Helpers.GetRandom(min_size, max_size);
+
+    if (this.DoesCollide(room)) {
+      i--;
+      continue;
+    }
+    room.w--;
+    room.h--;
+    this.rooms.push(room);
+  }
+
+
+
+  this.SquashRooms();
+
+      // Now we start building corridors between rooms that are near to one
+      // another. We choose a random point in each room and then move the second
+      // point towards the first one (in the while loop). We set each corridor
+      // tile to 1 which can later be interpreted by the dungeon drawing
+      // algorithm as such.
+
+  for (let i = 0; i < room_count; i++) {
+    let roomA = this.rooms[i];
+    let roomB = this.FindClosestRoom(roomA);
+
+    let pointA = {
+      x: Helpers.GetRandom(roomA.x, roomA.x + roomA.w),
+      y: Helpers.GetRandom(roomA.y, roomA.y + roomA.h)
+    };
+    let pointB = {
+      x: Helpers.GetRandom(roomB.x, roomB.x + roomB.w),
+      y: Helpers.GetRandom(roomB.y, roomB.y + roomB.h)
+    };
+
+    while ((pointB.x !== pointA.x) || (pointB.y !== pointA.y)) {
+      if (pointB.x !== pointA.x) {
+        if (pointB.x > pointA.x) pointB.x--;
+        else pointB.x++;
+      } else if (pointB.y !== pointA.y) {
+        if (pointB.y > pointA.y) pointB.y--;
+        else pointB.y++;
+      }
+
+      this.map[pointB.x][pointB.y] = 1;
+    }
+  }
+
+      // Here we iterate through all the rooms and set the tile to 1 for every
+      // tile within a room.
+
+  for (let i = 0; i < room_count; i++) {
+    let room = this.rooms[i];
+    for (let x = room.x; x < room.x + room.w; x++) {
+      for (let y = room.y; y < room.y + room.h; y++) {
+          this.map[x][y] = 1;
+      }
+    }
+  }
+
+
+      // This last part iterates through all the tiles in the map and if it
+      // finds a tile that is a floor (equal to 1) we check all the surrounding
+      // tiles for empty values. If we find an empty tile (that touches the floor)
+      // we build a wall (equal to 0).
+
+  for (let x = 0; x < this.map_size; x++) {
+    for (let y = 0; y < this.map_size; y++) {
+      if (this.map[x][y] === 1) {
+        for (let xx = x - 1; xx <= x + 1; xx++) {
+          for (let yy = y - 1; yy <= y + 1; yy++) {
+              if (this.map[xx][yy] === 999) this.map[xx][yy] = 0;
+          }
+        }
+      }
+    }
+  }
+};
 
 Dungeon.prototype.DoesCollide = function (room, ignore) {
   for (let i = 0; i < this.rooms.length; i++) {
@@ -64,101 +161,5 @@ Dungeon.prototype.FindClosestRoom = function (room) {
   }
   return closest;
 };
-
-Dungeon.prototype.Generate = function () {
-  this.map = [];
-  for (let x = 0; x < this.map_size; x++) {
-    this.map[x] = [];
-    for (let y = 0; y < this.map_size; y++) {
-        this.map[x][y] = 999;
-    }
-  }
-
-  let room_count = Helpers.GetRandom(3, 10);
-  let min_size = 3;
-  let max_size = 12;
-
-  for (let i = 0; i < room_count; i++) {
-    let room = {};
-
-    room.x = Helpers.GetRandom(1, this.map_size - max_size - 1);
-    room.y = Helpers.GetRandom(1, this.map_size - max_size - 1);
-    room.w = Helpers.GetRandom(min_size, max_size);
-    room.h = Helpers.GetRandom(min_size, max_size);
-
-    if (this.DoesCollide(room)) {
-      i--;
-      continue;
-    }
-    room.w--;
-    room.h--;
-
-    this.rooms.push(room);
-  }
-
-  this.SquashRooms();
-
-      // Now we start building corridors between rooms that are near to one
-      // another. We choose a random point in each room and then move the second
-      // point towards the first one (in the while loop). We set each corridor
-      // tile to 1 which can later be interpreted by the dungeon drawing
-      // algorithm as such.
-
-  for (let i = 0; i < room_count; i++) {
-    let roomA = this.rooms[i];
-    let roomB = this.FindClosestRoom(roomA);
-
-    let pointA = {
-      x: Helpers.GetRandom(roomA.x, roomA.x + roomA.w),
-      y: Helpers.GetRandom(roomA.y, roomA.y + roomA.h)
-    };
-    let pointB = {
-      x: Helpers.GetRandom(roomB.x, roomB.x + roomB.w),
-      y: Helpers.GetRandom(roomB.y, roomB.y + roomB.h)
-    };
-
-    while ((pointB.x !== pointA.x) || (pointB.y !== pointA.y)) {
-      if (pointB.x !== pointA.x) {
-        if (pointB.x > pointA.x) pointB.x--;
-        else pointB.x++;
-      } else if (pointB.y !== pointA.y) {
-        if (pointB.y > pointA.y) pointB.y--;
-        else pointB.y++;
-      }
-
-      this.map[pointB.x][pointB.y] = 1;
-    }
-  }
-      // Here we iterate through all the rooms and set the tile to 1 for every
-      // tile within a room.
-
-  for (let i = 0; i < room_count; i++) {
-    let room = this.rooms[i];
-    for (let x = room.x; x < room.x + room.w; x++) {
-      for (let y = room.y; y < room.y + room.h; y++) {
-          this.map[x][y] = 1;
-      }
-    }
-  }
-
-      // This last part iterates through all the tiles in the map and if it
-      // finds a tile that is a floor (equal to 1) we check all the surrounding
-      // tiles for empty values. If we find an empty tile (that touches the floor)
-      // we build a wall (equal to 0).
-
-  for (let x = 0; x < this.map_size; x++) {
-    for (let y = 0; y < this.map_size; y++) {
-      if (this.map[x][y] === 1) {
-        for (let xx = x - 1; xx <= x + 1; xx++) {
-          for (let yy = y - 1; yy <= y + 1; yy++) {
-              if (this.map[xx][yy] === 999) this.map[xx][yy] = 0;
-          }
-        }
-      }
-    }
-  }
-};
-
-
 
 export default Dungeon
--- src/reducers/helpers.js
+++ src/reducers/helpers.js
@@ -2,27 +2,6 @@ import { Game } from '../preload/initialState'
 import { items, enemies, weapons, boss } from '../preload/gameObjects';
 import deepFreeze from 'deep-freeze';
 
-// function updateObject(oldObject, newValues) {
-//     // Encapsulate the idea of passing a new object as the first parameter
-//     // to Object.assign to ensure we correctly copy data instead of mutating
-//     return Object.assign({}, oldObject, newValues);
-// }
-
-// function updateItemInArray(array, itemId, updateItemCallback) {
-//     const updatedItems = array.map(item => {
-//         if(item.id !== itemId) {
-//             // Since we only want to update one item, preserve all others as they are now
-//             return item;
-//         }
-//
-//         // Use the provided callback to create an updated item
-//         const updatedItem = updateItemCallback(item);
-//         return updatedItem;
-//     });
-//
-//     return updatedItems;
-// }
-
 function reachedGoal(grid,coords){
   return grid[coords[0]][coords[1]] === 5
 }
