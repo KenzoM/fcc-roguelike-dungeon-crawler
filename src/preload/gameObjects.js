127190798937410e17be185715784e5e1e368c02
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-14 14:10:50 -0800

    updated gameObjects.js

--- src/preload/gameObjects.js
+++ src/preload/gameObjects.js
@@ -16,19 +16,22 @@ const weapons = [
 
 const items = [
   [
-    { name: "banana", health: 20, coords: null },
-    { name: "banana", health: 20, coords: null },
-    { name: "avocado", health: 40, coords: null }
+    { name: "cheese-stick", health: 20, coords: null },
+    { name: "yogurt", health: 30, coords: null },
+    { name: "banana", health: 40, coords: null },
+    { name: "avocado", health: 60, coords: null }
   ],
   [
-    { name: "bacon", health: 50, coords: null },
-    { name: "sandwich", health: 60, coords: null },
-    { name: "pizza", health: 80, coords: null }
+    { name: "bacon", health: 60, coords: null },
+    { name: "sandwich", health: 100, coords: null },
+    { name: "pizza", health: 100, coords: null },
+    { name: "spaggeti", health: 130, coords: null }
   ],
   [
-    { name: "bacon", health: 50, coords: null },
-    { name: "pizza", health: 80, coords: null },
-    { name: "steak", health: 100, coords: null }
+    { name: "bacon", health: 60, coords: null },
+    { name: "pizza", health: 100, coords: null },
+    { name: "steak", health: 100, coords: null },
+    { name: "roasted-chicken", health: 150, coords: null }
   ]
 ]
 
@@ -40,20 +43,20 @@ const enemies = [
     {health: 100, level: 1, exp: 50, strength: 10}
   ],
   [
-    {health: 300, level: 2, exp: 50, strength: 10},
-    {health: 400, level: 2, exp: 50, strength: 10},
-    {health: 400, level: 2, exp: 50, strength: 10},
-    {health: 500, level: 2, exp: 50, strength: 10}
+    {health: 300, level: 2, exp: 50, strength: 20},
+    {health: 400, level: 2, exp: 50, strength: 23},
+    {health: 400, level: 2, exp: 50, strength: 26},
+    {health: 500, level: 2, exp: 50, strength: 30}
   ],
   [
-    {health: 500, level: 3, exp: 50, strength: 10},
-    {health: 600, level: 3, exp: 50, strength: 10},
-    {health: 700, level: 3, exp: 50, strength: 10},
-    {health: 700, level: 3, exp: 50, strength: 10}
+    {health: 500, level: 3, exp: 50, strength: 30},
+    {health: 600, level: 3, exp: 50, strength: 33},
+    {health: 700, level: 3, exp: 50, strength: 36},
+    {health: 700, level: 3, exp: 50, strength: 40}
   ]
 ]
 
-const boss = {health: 1000, strength: 500}
+const boss = {health: 500, strength: 100}
 
 
 export { weapons, items, enemies, boss }
