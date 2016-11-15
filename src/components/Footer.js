fa666f0941e7233978249a5d9641f465bd854e7e
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-15 11:23:18 -0800

    updated url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return(
       <div className="footer">
         <p>Coded by <a href="https://github.com/knowellG">Noel</a> & <a href="https://github.com/neotriz">Kenzo</a> </p>
-        <p><a href="https://github.com/knowellG/fcc-roguelike-dungeon-crawler">Source Code</a></p>
+        <p><a href="https://github.com/Neotriz/fcc-roguelike-dungeon-crawler">Source Code</a></p>
       </div>
     )
   }
