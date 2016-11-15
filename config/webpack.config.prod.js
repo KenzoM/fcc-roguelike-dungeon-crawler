73c69f66dd64b09d96d297ec19c22402dcbe8940
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-15 10:44:23 -0800

    npm build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -86,7 +86,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -103,7 +103,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -117,6 +117,11 @@ module.exports = {
       // tags. If you use code splitting, however, any async bundles will still
       // use the "style" loader inside the async code so CSS from them won't be
       // in the main CSS file.
+      {
+        test: /\.(sass|scss)$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "sass", "postcss"]
+      },
       {
         test: /\.css$/,
         // "?-autoprefixer" disables autoprefixer in css-loader itself:
@@ -157,7 +162,7 @@ module.exports = {
       }
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
