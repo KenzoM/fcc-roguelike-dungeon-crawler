062ddfd4b32b5dedf228929d14aa6dc0ac177cd2
Kenzo Mendoza <kenzom954@gmail.com>
2016-11-14 16:31:35 -0800

    updated app including adding footer and scss styles

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+
+  render(){
+    return(
+      <div className="footer">
+        <p>Coded by <a href="https://github.com/knowellG">Noel</a> & <a href="https://github.com/neotriz">Kenzo</a> </p>
+        <p><a href="https://github.com/knowellG/fcc-roguelike-dungeon-crawler">Source Code</a></p>
+      </div>
+    )
+  }
+}
